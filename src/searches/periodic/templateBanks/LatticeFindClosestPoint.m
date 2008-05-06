fb6b11a05b495165db1ee54406d95de047312d38
Reinhard Prix <Reinhard.Prix@ligo.org>
2008-05-06 22:15:08 +0200

    added analogous (trivial) functions for Zn and 'generic' lattice wrappers
    - this allows us to pick the lattice-type via input argument to
      generic wrappers

---
+++ src/searches/periodic/templateBanks/LatticeFindClosestPoint.m
@@ -0,0 +1,46 @@
+%% return the closest point of the lattice to the given point x in R^n
+%% This is just a wrapper to the lower-level lattice-specific functions.
+%% lattice is one of the strings { "Zn", "An", "Ans" }
+
+%%
+%% Copyright (C) 2008 Reinhard Prix
+%%
+%%  This program is free software; you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation; either version 2 of the License, or
+%%  (at your option) any later version.
+%%
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with with program; see the file COPYING. If not, write to the
+%%  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+%%  MA  02111-1307  USA
+%%
+
+function closest = LatticeFindClosestPoint ( dim, lattice )
+
+  valid = { "Zn", "An", "Ans" };
+
+  if ( strcmp ( lattice, valid{1}) )		%% Zn
+    closest = ZnFindClosestPoint ( dim );
+    return;
+  elseif ( strcmp ( lattice, valid{2} ) ) 	%% An
+    closest = AnFindClosestPoint ( dim );
+    return;
+  elseif ( strcmp ( lattice, valid{3} ) )	%% An*
+    closest = AnsFindClosestPoint ( dim );
+    return;
+  else
+    printf ("Unknown lattice-type, must be one of: ");
+    printf (" '%s',", valid{1:length(valid)} );
+    printf ("\n");
+    error ("Illegal input.\n");
+  endif
+
+  return;
+
+endfunction
---
+++ src/searches/periodic/templateBanks/LatticeGenerator.m
@@ -0,0 +1,46 @@
+%% return an nxn full-rank generating matrix for the specified lattice
+%% This is just a wrapper to the lower-level lattice-specific functions.
+%% lattice is one of the strings { "Zn", "An", "Ans" }
+
+%%
+%% Copyright (C) 2008 Reinhard Prix
+%%
+%%  This program is free software; you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation; either version 2 of the License, or
+%%  (at your option) any later version.
+%%
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with with program; see the file COPYING. If not, write to the
+%%  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+%%  MA  02111-1307  USA
+%%
+
+function [ gen, rot ] = LatticeGenerator ( dim, lattice )
+
+  valid = { "Zn", "An", "Ans" };
+
+  if ( strcmp ( lattice, valid{1}) )		%% Zn
+    [gen, rot] = ZnGenerator ( dim );
+    return;
+  elseif ( strcmp ( lattice, valid{2} ) ) 	%% An
+    [gen, rot] = AnGenerator ( dim );
+    return;
+  elseif ( strcmp ( lattice, valid{3} ) )	%% An*
+    [gen, rot] = AnsGenerator ( dim );
+    return;
+  else
+    printf ("Unknown lattice-type, must be one of: ");
+    printf (" '%s',", valid{1:length(valid)} );
+    printf ("\n");
+    error ("Illegal input.\n");
+  endif
+
+  return;
+
+endfunction
