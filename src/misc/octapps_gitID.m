9f1309ce25892ad6b6be06c06b511b37963570fa
John T. Whelan <john.whelan@ligo.org>
2008-11-01 17:28:33 +0100

    incorporated changes from lal/lalapps gitID, including status and making date UTC

--- src/misc/octapps_gitID.m
+++ src/misc/octapps_gitID.m
@@ -1,18 +1,18 @@
 %% ret = octapps_gitID()
 %%
-%% Return the current HEAD git-tag (commitID, dateID, commit-string)
+%% Return the current HEAD git-tag (commitID, date, etc)
 %% of OCTAPPS.
 %%
 %% If the optional argument 'run_local' == true, then
 %% determine the git-tag of the *local* directory in which the
 %% current calling script is running. (default == false)
 %%
-%% returns struct with fields {fullID, commitID, commitDate, commitAuthor, commitTitle}
-%% where 'fullID' is simply the unparsed ID string
+%% returns struct with fields {fullID, commitID, commitDate, commitAuthor, commitTitle, gitStatus}
+%% where 'fullID' is a nicely-formatted concatenation of the individual fields
 %%
 
 %%
-%% Copyright (C) 2008 Reinhard Prix
+%% Copyright (C) 2008 Reinhard Prix, John Whelan
 %%
 %%  This program is free software; you can redistribute it and/or modify
 %%  it under the terms of the GNU General Public License as published by
@@ -49,33 +49,79 @@ function ret = octapps_gitID ( run_local )
     endif
     origdir = orig ( 1:(ind -1) );
 
-    logcmd = strcat ( "cd ", origdir, " && " );
+    cdcmd = strcat ( "cd ", origdir, " && " );
   else
-    logcmd = "";
+    cdcmd = "";
   endif	## !run_local
 
-  fmt = "format:CommitID: %H%nCommitDate: %aD%nCommitAuthor: %ae%nCommitTitle: %s";
-  logcmd = sprintf ( "%s git-log -1 --pretty='%s'", logcmd, fmt );
+  ## ---------- read out git-log of last commit --------------------
+  ## This method matches what's used in lal/lalapps, but could fall
+  ## victim to a race condition.
+  fmt_id = "format:%H";
+  logcmd_id = sprintf( "%sgit log -1 --pretty='%s'", cdcmd, fmt_id );
+  fmt_udate="format:%at";
+  logcmd_udate = sprintf( "%sgit log -1 --pretty='%s'", cdcmd, fmt_udate );
+  fmt_author="format:%ae";
+  logcmd_author = sprintf( "%sgit log -1 --pretty='%s'", cdcmd, fmt_author );
+  fmt_title="format:%s";
+  logcmd_title = sprintf( "%sgit log -1 --pretty='%s'", cdcmd, fmt_title );
 
-  [err, gitid ] = system29 ( logcmd );
+  [ err, git_id ] = system29 ( logcmd_id );
   if ( err )
-    error ("Failed to get git-ID, error was %d\n", err );
+    ## we seem to be unable to run git; presumably we were not checked
+    ## out of a repo
+    git_id = "unknown.";
+    git_author = "unknown.";
+    git_title = "unknown.";
+## If date unknown, use GPS 0 so it can still be parsed
+    git_date_utc = "1980-01-06 00:00:00 UTC";
+  else
+    [ err, git_id ] = system29 ( logcmd_id );
+    if ( err )
+      error ("Unexpectedly failed to get git-ID, error was %d\n", err );
+    endif
+    [ err, git_udate ] = system29 ( logcmd_udate );
+    if ( err )
+      error ("Unexpectedly failed to get git-date, error was %d\n", err );
+    endif
+    mytime = gmtime(str2num(git_udate));
+    git_date_utc = sprintf("%04d-%02d-%02d %02d:%02d:%02d UTC",
+			   1900 + mytime.year, 1 + mytime.mon, mytime.mday,
+			   mytime.hour, mytime.min, mytime.sec);
+    [ err, git_author ] = system29 ( logcmd_author );
+    if ( err )
+      error ("Unexpectedly failed to get git-author, error was %d\n", err );
+    endif
+    [ err, git_title ] = system29 ( logcmd_title );
+    if ( err )
+      error ("Unexpectedly failed to get git-title, error was %d\n", err );
+    endif
   endif
 
-  ind0 = index ( gitid, "CommitID: ");
-  ind1 = index ( gitid, "CommitDate: ");
-  ind2 = index ( gitid, "CommitAuthor: ");
-  ind3 = index ( gitid, "CommitTitle: ");
+  statuscmd = sprintf( "%sgit status -a", cdcmd );
+  [ err, msg ] = system29 ( statuscmd );
 
-  if ( !ind0 || !ind1 || !ind2 || !ind3 )
-    error ("Failed to parse git-id string '%s'\n", gitid );
-  endif
+  ## Three possibilities:
+  if ( err )
+    if ( msg )
+      ## non-zero error code, with output: no changes to be committed
+      git_status = "CLEAN. All modifications commited."
+    else
+      ## non-zero error code, empty output: call to git status failed
+      git_status = "unknown."
+    endif
+  else
+    ## zero error code: changes to be committed
+    git_status = "UNCLEAN: some modifications were not commited!";
+ endif
 
-  ret.fullID       = gitid;	%% full unparsed ID string
-  ret.commitID     = deblank(gitid ( ind0+length("CommitID: "):ind1-1 ));
-  ret.commitDate   = deblank(gitid ( ind1+length("CommitDate: "):ind2-1 ));
-  ret.commitAuthor = deblank(gitid ( ind2+length("CommitAuthor: "):ind3-1 ));
-  ret.commitTitle  = deblank(gitid ( ind3+length("CommitTitle: "):end ));
-
-
+  ret.commitID = sprintf("$CommitID: %s$", git_id);
+  ret.commitDate = sprintf("$CommitDate: %s$", git_date_utc);
+  ret.commitAuthor = sprintf("$CommitAuthor: %s$", git_author);
+  ## Should clean up the title in case it has a $ or something
+  ret.commitTitle = sprintf("$CommitTitle: %s$", git_title);
+  ret.gitStatus = sprintf("$GitStatus: %s$", git_status);
+  ret.fullID = sprintf("     %s\n     %s\n     %s\n     %s\n     %s\n",
+		       ret.commitID, ret.commitDate, ret.commitAuthor,
+		       ret.commitTitle, ret.gitStatus);
 endfunction
