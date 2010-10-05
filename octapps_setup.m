6630aec7c7127487f3a688a2cab1c5434ca614c7
Karl Wette <karl.wette@ligo.org>
2010-10-05 16:55:05 +0200

    New/alternative setup script for octave, written in octave
    Can be used either from the shell to print the relevant bash/csh
    commands to eval, or from within octave itself to update the path.
    Doesn't rely on a fixed list of path, instead searches octapps
    for all directories containing *.m and *.oct files

---
+++ octapps_setup.m
@@ -0,0 +1,149 @@
+#!/usr/bin/octave -qf
+##
+## Setup script for OCTAPPS paths.
+## It can be run in a shell:
+##    eval `/path/to/octapps_setup.m <name of shell>`
+## or within Octave:
+##    octave_setup
+##
+
+##
+##  Copyright (C) 2010 Karl Wette
+##
+##  This program is free software; you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation; either version 2 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with with program; see the file COPYING. If not, write to the
+##  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+##  MA  02111-1307  USA
+##
+
+## this is a script
+1;
+
+## put everything in an internal function, so workspace
+## isn't contaminated when run from within Octave
+function octapps_setup_function
+
+  ## this must match the name of this file!
+  my_name = "octapps_setup.m";
+
+  ## if run as a script, program_name == my_name
+  ## if run within Octave, program_name == 'octave'
+  run_as_script = strcmp(program_name, my_name);
+
+  if run_as_script
+
+    ## check for name of shell on command line
+    if length(argv) != 1
+      printf("Usage: %s <name of shell>", my_name);
+      return;
+    endif
+    
+    ## get the filename component
+    [shell{1:2}] = fileparts(argv(){1});
+    shell = shell{2};
+
+  endif
+
+  ## get the absolute path to this file
+  if run_as_script
+    my_path = program_invocation_name;
+  else
+    my_path = which(my_name);
+  endif
+  if my_path(1) == filesep
+    my_path = canonicalize_file_name(my_path);
+  else
+    my_path = canonicalize_file_name(fullfile(pwd, my_path));
+  endif
+
+  ## get the directory containing this file, which
+  ## is assumed to be the OCTAPPS root directory
+  my_path = fileparts(my_path);
+
+  ## recusively search all subdirectories in OCTAPPS,
+  ## looking for directories containing *.{m,oct} files
+  dirs = {my_path};
+  octapps_path = {};
+  while length(dirs) > 0
+    d = dirs{1};
+    dirs(1) = [];
+    files = dir(d);
+    for i = 1:length(files)
+      if files(i).isdir && files(i).name(1) != "."
+	dirs{end+1} = strcat(d, filesep, files(i).name);
+      endif
+    endfor
+    octfiles = 0;
+    for i = {"*.m", "*.oct"}
+      octfiles += length(glob(fullfile(d, i{1})));
+    endfor
+    if octfiles > 0
+      octapps_path{end+1} = d;
+    endif
+  endwhile
+  
+  ## get the full Octave path
+  octave_path = strsplit(path, pathsep, true);
+
+  ## get the Octave path set by the environment
+  octave_path_env = strsplit(getenv("OCTAVE_PATH"), pathsep, true);
+
+  ## remove any existing OCTAPPS directories
+  ## from the Octave full/environment paths
+  for i = 1:length(octapps_path)
+    j = strcmp(octave_path, octapps_path{i});
+    if !isempty(j)
+      octave_path(j) = [];
+    endif
+    j = strcmp(octave_path_env, octapps_path{i});
+    if !isempty(j)
+      octave_path_env(j) = [];
+    endif
+  endfor
+
+  ## add the OCTAPPS directories to the Octave path
+  ## (these will persist if this script is run within Octave)
+  ## make sure "." stays at the top of the path
+  if strcmp(octave_path{1}, ".")
+    path(octave_path{1}, octapps_path{:}, octave_path{2:end});
+  else
+    path(octapps_path{:}, octave_path{:});
+  endif
+
+  ## if run as a script, also print
+  ## commands to be eval'd within a shell
+  if run_as_script
+    
+    ## build the shell environment string
+    octave_path_env = {octapps_path{:}, octave_path_env{:}};
+    octave_path_env_str = "";
+    for i = 1:length(octave_path_env)
+      octave_path_env_str = strcat(octave_path_env_str, pathsep, octave_path_env{i});
+    endfor
+    
+    ## print the shell-appropriate command
+    switch shell
+      case {"bash"}
+	printf("export OCTAVE_PATH=%s", octave_path_env_str);
+      case {"csh" "tcsh"}
+	printf("setenv OCTAVE_PATH %s", octave_path_env_str);
+      otherwise
+	error(["Unrecognised shell '" shell "'"]);
+    endswitch
+    
+  endif
+  
+endfunction
+
+## do it!
+octapps_setup_function
