f6e8de3661141b03aece1d189cbae5693f01eaca
Reinhard Prix <reinhard.prix@ligo.org>
2011-04-04 15:48:06 +0200

    extend SignalToNoiseRsqr() to take an optional mismatch-histogram
       - will generate SNRs with randomly drawn mismatches folded in

--- src/pulsar/sensitivity/SignalToNoiseRsqr.m
+++ src/pulsar/sensitivity/SignalToNoiseRsqr.m
@@ -5,31 +5,41 @@
 %% where:
 %%   apxsqr = joint histogram of normalised signal amplitudes
 %%   Fpxsqr = joint histogram of time-averaged beam patterns
+%%   mis    = histogram of mismatches
 %%   Rsqr   = histogram of "R^2" component of the optimal SNR
-function hRsqr = SignalToNoiseRsqr(hapxsqr, hFpxsqr)
+function hRsqr = SignalToNoiseRsqr(hapxsqr, hFpxsqr, hmis )
 
   %% check input
   assert(isHist(hapxsqr) && isHist(hFpxsqr));
+
+  if ( !exist("hmis") )
+    hmis = newHist({1});
+    hmis.xb{1} = 0;
+  endif
+  assert ( isHist(hmis) );
+
   hRsqr = newHist;
 
   %% if both histograms are "constant", return new histogram
-  if (isempty(hapxsqr.px) && isempty(hFpxsqr.px))
+  if (isempty(hapxsqr.px) && isempty(hFpxsqr.px) && isempty(hmis.px) )
     hRsqr.xb{1} = hapxsqr.xb{1}*hFpxsqr.xb{1} + hapxsqr.xb{2}*hFpxsqr.xb{2};
+    hRsqr.xb{1} *= ( 1 - hmis.xb{1} );	%% multiply by 1-mismatch
   else
 
     %% otherwise, build up histogram
     N = 20000;
     dx = 0.01;
     hRsqr = newHist;
-    apxsqrwksp = Fpxsqrwksp = [];
+    apxsqrwksp = Fpxsqrwksp = miswksp = [];
     do
 
       %% generate values of ap, ax, Fp, and Fx
       [apxsqr, apxsqrwksp] = drawFromHist(hapxsqr, N, apxsqrwksp);
       [Fpxsqr, Fpxsqrwksp] = drawFromHist(hFpxsqr, N, Fpxsqrwksp);
+      [mis, miswksp]       = drawFromHist(hmis,    N, miswksp);
 
-      %% calculate R^2 = ap^2*Fp^2 + ax^2*Fx^2
-      Rsqr = sum(apxsqr.*Fpxsqr, 2);
+      %% calculate R^2 = (1 - mis) * ( ap^2*Fp^2 + ax^2*Fx^2 )
+      Rsqr = ( 1 - mis ) .* sum(apxsqr.*Fpxsqr, 2);
 
       %% add new values to histogram
       oldhRsqr = hRsqr;
