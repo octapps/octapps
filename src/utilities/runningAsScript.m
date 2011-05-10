ddab19628d81ca694ec5afb3f6c6d1cde0726394
Karl Wette <karl.wette@ligo.org>
2011-05-10 15:58:39 +0200

    Combined command-line / function argument parser
    - Should do just about everything ...
    - Replaces parseOptionals and keyWords

---
+++ src/utilities/runningAsScript.m
@@ -0,0 +1,39 @@
+%% Returns whether Octave is currently executing
+%% a script, as opposed to an interactive session
+%% Syntax:
+%%   isscript = runningAsScript
+
+%%
+%%  Copyright (C) 2011 Karl Wette
+%%
+%%  This program is free software; you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation; either version 2 of the License, or
+%%  (at your option) any later version.
+%%
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%
+%%  You should have received a copy of the GNU General Public License
+%%  along with with program; see the file COPYING. If not, write to the
+%%  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+%%  MA  02111-1307  USA
+%%
+
+function isscript = runningAsScript
+
+  %% get filename of function at top of stack
+  stack = dbstack();
+  if length(stack) > 0
+    fname = stack(end).file;
+  else
+    fname = [];
+  endif
+
+  %% if filename equals program invocation name,
+  %% we're running a script
+  isscript = strcmp(fname, program_invocation_name);
+
+endfunction
