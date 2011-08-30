4f2b99f482e048c1ff6fb1cbd152eb6736ba0bf3
Karl Wette <karl.wette@ligo.org>
2011-08-30 20:39:27 +0200

    Updated random parameter generation functions
    - work with new SWIG module

--- src/pulsar/sensitivity/CreateRandParam.m
+++ src/pulsar/sensitivity/CreateRandParam.m
@@ -1,28 +1,45 @@
-%% Parses random parameters specs, which may be either
-%%   <constant>,     denoting a single value, or
-%%   [<min>, <max>], denoting a range of values
-%% Syntax:
-%%   rng = CreateQandParam(p, p, ...)
-%% where
-%%   rng = random parameter generator
-%%   p   = random parameter spec
+## Copyright (C) 2011 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
+
+## Parses random parameters specs, which may be either
+##   <constant>,     denoting a single value, or
+##   [<min>, <max>], denoting a range of values
+## Syntax:
+##   rng = CreateQandParam(p, p, ...)
+## where
+##   rng = random parameter generator
+##   p   = random parameter spec
+
 function rng = CreateRandParam(varargin)
 
-  %% load quasi-random number generator
+  ## load quasi-random number generator
   gsl_qrng;
-  global cvar;
 
-  %% initial indexes of random and constant parameters
+  ## initial indexes of random and constant parameters
   rng.rii = rng.rm = rng.rc = rng.cii = rng.cc = [];
 
-  %% iterate over parameters
+  ## iterate over parameters
   for i = 1:nargin
     p = varargin{i};
     switch numel(p)
-      case 1    % <constant>
+      case 1    # <constant>
 	rng.cii(end+1,1) = i;
 	rng.cc(end+1,1) = p;
-      case 2    % [<min>, <max>]
+      case 2    # [<min>, <max>]
 	rng.rii(end+1,1) = i;
 	rng.rm(end+1,1) = max(p) - min(p);
 	rng.rc(end+1,1) = min(p);
@@ -31,9 +48,9 @@ function rng = CreateRandParam(varargin)
     endswitch
   endfor
 
-  %% create quasi-random number generator if needed
+  ## create quasi-random number generator if needed
   if length(rng.rii) > 0
-    rng.q = new_gsl_qrng(cvar.gsl_qrng_halton, length(rng.rii));
+    rng.q = gsl_qrng_alloc(gsl_qrng.gsl_qrng_halton, length(rng.rii));
   endif
   rng.allconst = (length(rng.rii) == 0);
 
--- src/pulsar/sensitivity/NextRandParam.m
+++ src/pulsar/sensitivity/NextRandParam.m
@@ -1,29 +1,49 @@
-%% Generates values for random parameters, given a generator
-%% Syntax:
-%%   [v, v, ...] = NextRandParam(rng, N)
-%% where
-%%   rng = random parameter generator
-%%   N   = number of values to generate
-%%   v   = values of random parameter
+## Copyright (C) 2011 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
+
+## Generates values for random parameters, given a generator
+## Syntax:
+##   [v, v, ...] = NextRandParam(rng, N)
+## where
+##   rng = random parameter generator
+##   N   = number of values to generate
+##   v   = values of random parameter
+
 function varargout = NextRandParam(rng, N)
 
-  %% load quasi-random number generator
+  ## load quasi-random number generator
   gsl_qrng;
-  global cvar;
 
-  %% check input arguments
+  ## check input arguments
+  if !exist("N")
+    N = 1;
+  endif
   if nargout ~= length(rng.rii) + length(rng.cii);
     error("Incorrect number of output arguments!");
   endif
 
-  %% fill random parameters with next quasi-random vector
+  ## fill random parameters with next quasi-random vector
   if length(rng.rii) > 0
     r = gsl_qrng_get(rng.q, N);
     [varargout{rng.rii}] = deal(mat2cell(rng.rm(:,ones(N,1)) .* r + ...
 					 rng.rc(:,ones(N,1)),ones(length(rng.rii),1),N){:});
   endif
 
-  %% fill constant parameters
+  ## fill constant parameters
   if length(rng.cii) > 0
     [varargout{rng.cii}] = deal(mat2cell(rng.cc(:,ones(N,1)),ones(length(rng.cii),1),N){:});
   endif
