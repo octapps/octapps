fce3bb321d113c147d4d5f909e0ff70b41b375dc
Karl Wette <karl.wette@ligo.org>
2011-09-16 16:36:25 +0200

    Split command-line parsing and option parsing

---
+++ src/utilities/parseCommandLine.m
@@ -0,0 +1,141 @@
+## Copyright (C) 2011 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
+
+## Command line argument pre-parser: use with parseOptions to
+## call Octave functions from the command line.
+## Usage:
+##
+##   function MyScript(varargin)
+##     parseOptions(varargin, ...);
+##     ...
+##   endfunction
+##
+##   if runningAsScript
+##     MyScript(parseCommandLine(){:});
+##   endif
+##
+## Command-line options may be given either as
+##   '--name' 'value'
+## or as
+##   '--name=value'
+## is <name> does not accept a 'char' value (as determined by
+## parseOptions), <value> will be treated as an Octave expression
+## and eval()uated, surrounded by []s.
+
+function opts = parseCommandLine(varargin)
+
+  ## get command-line arguments
+  if length(varargin) > 0
+    args = varargin;   # for debugging purposes
+  else
+    args = argv();
+  endif
+
+  ## stupid Octave; if no command-line arguments are given to script, argv()
+  ## will contain the entire Octave command-line, instead of simply the
+  ## arguments after the script name! so we have to check for this
+  if length(args) > 0 && strcmp(args{end}, program_invocation_name)
+    args = {};
+  endif
+
+  ## print caller's usage if --help is given
+  if any(cellfun(@(x) strcmp(x, "--help"), args))
+    
+    ## get name of calling function
+    stack = dbstack();
+    if numel(stack) > 1
+      callername = stack(2).name;
+    else
+      error("No help information for %s\n", program_invocation_name);
+    endif
+    
+    ## get plain help text of calling function
+    [helptext, helpfmt] = get_help_text(callername);
+    if !strcmp(helpfmt, "plain text")
+      [helptext, helpstat] = __makeinfo__(helptext, "plain text");
+      if !helpstat
+        error("No plain-text help information for %s\n", program_invocation_name);
+      endif
+    endif
+    
+    ## remove shebang from help text
+    if strncmp(helptext, "!", 1)
+      i = min(strfind(helptext, "\n"));
+      if isempty(i)
+        error("No help information for %s\n", program_invocation_name);
+      else
+        helptext = helptext(i+1:end);
+      endif
+    endif
+    
+    ## print help text and exit
+    fprintf("\n%s\n", helptext);
+    error("Exiting %s after displaying help\n", program_invocation_name);
+    
+  endif
+  
+  ## parse command-line arguments
+  opts = {};
+  n = 1;
+  while n <= length(args)
+    arg = args{n++};
+    argvalstr = [];
+
+    ## if argument is '--', add it to options
+    if strcmp(arg, "--")
+      opts{end+1} = arg;
+      continue
+    endif
+    
+    ## check that argument begins with '--'
+    if strncmp(arg, "--", 2)
+      
+      ## if argument contains an '=', split into name=value,
+      i = min(strfind(arg, "="));
+      if !isempty(i)
+        argcmdname = arg(3:i-1);
+        argvalstr = arg(i+1:end);
+      else
+        ## otherwise just store the name
+        argcmdname = arg(3:end);
+      endif
+      
+    else
+      error("%s: Could not parse argument '%s'", funcName, arg);
+    endif
+    
+    ## replace '-' with '_' to make a valid Octave variable name
+    argname = strrep(argcmdname, "-", "_");
+    
+    ## if no argument value string has been found yet, check next argument
+    if isempty(argvalstr) && n <= length(args)
+      nextarg = args{n++};
+      ## if next argument isn't itself an argument, use as a value string
+      if !strncmp(nextarg, "--", 2)
+        argvalstr = nextarg;
+      endif
+    endif
+    if isempty(argvalstr)
+      error("%s: Could to determine the value of argument '%s'", funcName, argcmdname);
+    endif
+
+    ## add argument and value to options
+    opts = {opts{:}, argname, argvalstr};
+
+  endwhile
+
+endfunction
--- src/utilities/parseOptions.m
+++ src/utilities/parseOptions.m
@@ -19,7 +19,7 @@
 ## Syntax:
 ##   paropts = parseOptions(opts, optspec, optspec, ...)
 ## where:
-##   opts    = command-line / function options
+##   opts    = function options
 ##   optspec = option specification, one of:
 ##      * required option:  {'name','types'}
 ##      * optional option:  {'name','types',defvalue}
@@ -28,7 +28,7 @@
 ##         types    = datatype specification of option:
 ##                    'type,type,...'
 ##         defvalue = default value given to <name>
-##   paropts = parsed command-line / function options (optional)
+##   paropts = parsed function options (optional)
 ## Notes:
 ##   * <name> will be assigned values in the context of
 ##     the calling function, unless paropts is given
@@ -38,18 +38,7 @@
 ##        <types> = 'numeric,scalar'
 ##     then a value <x> must satisfy:
 ##        isnumeric(x) && isscalar(x)
-##   * if runningAsScript == true, parseOptions assumes
-##     that opts contains command-line options from argv().
-##     Command-line options may be given either as
-##        '--name' 'value'
-##     or as
-##        '--name=value'
-##     If the <type> of <name> is 'char', and <value> does
-##     not begin with a [, it will be treated as a string;
-##     otherwise it will be treated as an Octave expression
-##     and evaluated with eval(), surrounded by []s.
-##   * otherwise, parseOptions assumes that opts contains
-##     function arguments from varargin, of the form:
+##   * <opts> should contain options of the form
 ##        reg,reg,...,"key",val,"key",val,...
 ##     where <reg> are regular options and <key>-<val> are
 ##     keyword-value option pairs. Regular options are
@@ -112,177 +101,69 @@ function paropts = parseOptions(opts, varargin)
     endif
 
   endfor
-
-  ## if running as a script
-  if runningAsScript
-
-    ## stupid Octave; if no command-line options are given to script, argv()
-    ## will contain the entire Octave command-line, instead of simply the
-    ## options after the script name! so we have to check for this
-    if length(opts) > 0 && strcmp(opts{end}, program_invocation_name)
-      opts = {};
-    endif
-
-    ## print caller's usage if --help is given
-    if any(cellfun(@(x) strcmp(x, "--help"), opts))
-
-      ## get name of calling function
-      stack = dbstack();
-      if numel(stack) > 1
-        callername = stack(2).name;
-      else
-        error("No help information for %s\n", program_invocation_name);
-      endif
-
-      ## get plain help text of calling function
-      [helptext, helpfmt] = get_help_text(callername);
-      if !strcmp(helpfmt, "plain text")
-        [helptext, helpstat] = __makeinfo__(helptext, "plain text");
-        if !helpstat
-          error("No plain-text help information for %s\n", program_invocation_name);
-        endif
-      endif
-
-      ## remove shebang from help text
-      if strncmp(helptext, "!", 1)
-        i = min(strfind(helptext, "\n"));
-        if isempty(i)
-          error("No help information for %s\n", program_invocation_name);
-        else
-          helptext = helptext(i+1:end);
-        endif
-      endif
-
-      ## print help text and exit
-      fprintf("\n%s\n", helptext);
-      error("Exiting %s after displaying help\n", program_invocation_name);
-
-    endif
-
-    ## parse command-line options
-    n = 1;
-    while n <= length(opts)
-      opt = opts{n++};
-      optvalstr = [];
-
-      ## check that option begins with '--'
-      if strncmp(opt, "--", 2)
-
-        ## if option contains an '=', split into name=value,
-        i = min(strfind(opt, "="));
-        if !isempty(i)
-          optcmdname = opt(3:i-1);
-          optvalstr = opt(i+1:end);
-        else
-          ## otherwise just store the name
-          optcmdname = opt;
-        endif
-
-      else
-        error("%s: Could not parse option '%s'", funcName, opt);
-      endif
-
-      ## replace '-' with '_' to make a valid Octave variable name
-      optname = strrep(optcmdname, "-", "_");
-
-      ## check that this option is an allowed option
-      if !isfield(allowed, optname)
-        error("%s: Unknown option '%s'", funcName, optcmdname);
-      endif
-
-      ## if no option value string has been found yet, check next option
-      if isempty(optvalstr) && n <= length(opts)
-        nextopt = opts{n++};
-        ## if next option isn't itself an option, use as a value string
-        if !strncmp(nextopt, "--", 2)
-          optvalstr = nextopt;
-        endif
-      endif
-      if isempty(optvalstr)
-        error("%s: Could to determine the value of option '%s'", funcName, optcmdname);
-      endif
-
-      ## if option accepts a 'char' and its value string doesn't begin with '['
-      if feval(typefunc.(optname), "string") && !strncmp(optvalstr, "[", 1)
-        ## assign directly to option value
-        optvalue = optvalstr;
-      else
-        ## parse option value string as an Octave expression
-        try
-          eval(sprintf("optvalue=[%s];", optvalstr));
-        catch
-          error("%s: Could not create a value from '%s'", funcName, optvalstr);
-        end_try_catch
-      endif
-
-      ## assign option value, if it's the right type
-      if !feval(typefunc.(optname), optvalue)
-        error("%s: Value of '%s' must satisfy: %s", funcName, optcmdname, formula(typefunc.(optname)));
-      endif
-      paropts.(optname) = optvalue;
-
-      ## mark that this option has been used
-      --allowed.(optname);
-      if isfield(required, optname)
-        --required.(optname);
-      endif
-
-    endwhile
-
-  else
-
-    ## split function arguments into regular options and keyword-value pairs
-    [regopts, kvopts] = parseparams(opts);
-
-    ## check if there's more regular options than required options
-    if length(regopts) > length(reqnames)
-      error("%s: Too many regular arguments; maximum is %i", funcName, length(reqnames))
-    endif
-
-    ## assign regular options in order given by 'reqnames'
-    for n = 1:length(regopts)
-
-      ## assign option value, if it's the right type
-      if !feval(typefunc.(reqnames{n}), regopts{n})
-        error("%s: Value of '%s' must satisfy: %s", funcName, reqnames{n}, formula(typefunc.(reqnames{n})));
-      endif
-      paropts.(reqnames{n}) = regopts{n};
-
-      ## mark that this option has been used
-      --allowed.(reqnames{n});
-      --required.(reqnames{n});
-
-    endfor
-
-    ## check that there's an even number of items in the keyword-value list
-    if mod(length(kvopts), 2) != 0
-      error("%s: Expected 'key',value pairs following regular options in args", funcName);
-    endif
-
-    ## assign keyword-value options
-    for n = 1:2:length(kvopts)
-
-      ## check that this option is an allowed option
-      if !isfield(allowed, kvopts{n})
-        error("%s: Unknown option '%s'", funcName, kvopts{n});
-      endif
-
-      ## assign option value, if it's the right type
-      if !feval(typefunc.(kvopts{n}), kvopts{n+1})
-        error("%s: Value of '%s' must satisfy: %s", funcName, kvopts{n}, formula(typefunc.(kvopts{n})));
-      endif
-      paropts.(kvopts{n}) = kvopts{n+1};
-
-      ## mark that this option has been used
-      --allowed.(kvopts{n});
-      if isfield(required, kvopts{n})
-        --required.(kvopts{n});
-      endif
-
-    endfor
-
+      
+  ## split function arguments into regular options and keyword-value pairs
+  [regopts, kvopts] = parseparams(opts);
+  
+  ## check if there's more regular options than required options
+  if length(regopts) > length(reqnames)
+    error("%s: Too many regular arguments; maximum is %i", funcName, length(reqnames))
   endif
+  
+  ## assign regular options in order given by 'reqnames'
+  for n = 1:length(regopts)
+    
+    ## assign option value, if it's the right type
+    if !feval(typefunc.(reqnames{n}), regopts{n})
+      error("%s: Value of '%s' must satisfy: %s", funcName, reqnames{n}, formula(typefunc.(reqnames{n})));
+    endif
+    paropts.(reqnames{n}) = regopts{n};
+    
+    ## mark that this option has been used
+    --allowed.(reqnames{n});
+    --required.(reqnames{n});
+    
+  endfor
+  
+  ## check that there's an even number of items in the keyword-value list
+  if mod(length(kvopts), 2) != 0
+    error("%s: Expected 'key',value pairs following regular options in args", funcName);
+  endif
+  
+  ## assign keyword-value options
+  for n = 1:2:length(kvopts)
+    optkey = kvopts{n};
+    optval = kvopts{n+1};
 
+    ## check that this option is an allowed option
+    if !isfield(allowed, optkey)
+      error("%s: Unknown option '%s'", funcName, optkey);
+    endif
+
+    ## if option does not accept a 'char' value, but option value is a 'char',
+    ## try evaluating it (this is used when parsing arguments from the command line)
+    if !feval(typefunc.(optkey), "string") && ischar(optval)
+      try
+        eval(sprintf("optval=[%s];", optval));
+      catch
+        error("%s: Could not create a value from '%s'", funcName, optvalstr);
+      end_try_catch
+    endif
+
+    ## assign option value, if it's the right type
+    if !feval(typefunc.(optkey), optval)
+      error("%s: Value of '%s' must satisfy: %s", funcName, optkey, formula(typefunc.(optkey)));
+    endif
+    paropts.(optkey) = optval;
+    
+    ## mark that this option has been used
+    --allowed.(optkey);
+    if isfield(required, optkey)
+      --required.(optkey);
+    endif
+    
+  endfor
+  
   ## check that options have been used correctly
   allnames = fieldnames(allowed);
   for n = 1:length(allnames)
