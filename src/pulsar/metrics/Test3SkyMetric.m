146b2ddcf867bfa9ca82020c519b5a99a8ec861f
Karl Wette <karl.wette@ligo.org>
2013-03-12 20:56:22 +0100

    More work on Test3SkyMetric

--- src/pulsar/metrics/Test3SkyMetric.m
+++ src/pulsar/metrics/Test3SkyMetric.m
@@ -25,9 +25,9 @@ function results = Test3SkyMetric(varargin)
                {"detectors", "char"},
                {"ephem_year", "char", []},
                {"fiducial_freq", "real,strictpos,scalar"},
-               {"ptolemaic", "logical,scalar", []},
+               {"sky_projection", "char"},
                {"max_mismatch", "real,strictpos,scalar", 0.5},
-               {"num_trials", "integer,strictpos,scalar", 1e6},
+               {"num_trials", "integer,strictpos,scalar"},
                []);
 
   ## load LAL libraries
@@ -35,7 +35,25 @@ function results = Test3SkyMetric(varargin)
   lalpulsar;
 
   ## bin widths for mismatch histograms
-  lgerr_H_dx = 0.1;
+  relax_H_dx = 0.01;
+
+  ## create linear phase model metrics
+  [metricLPI, coordIDsLPI] = CreatePhaseMetric("coords", "ssky_equ,freq,fdots",
+                                               "spindowns", spindowns,
+                                               "start_time", start_time,
+                                               "ref_time", ref_time,
+                                               "time_span", time_span,
+                                               "detectors", detectors,
+                                               "fiducial_freq", fiducial_freq,
+                                               "phase_model", "linearI");
+  [metricLPII, coordIDsLPII] = CreatePhaseMetric("coords", "ssky_equ,freq,fdots",
+                                                 "spindowns", spindowns,
+                                                 "start_time", start_time,
+                                                 "ref_time", ref_time,
+                                                 "time_span", time_span,
+                                                 "detectors", detectors,
+                                                 "fiducial_freq", fiducial_freq,
+                                                 "phase_model", "linearII");
 
   ## create 2+3-sky metric
   [metric2p3, coordIDs2p3] = CreatePhaseMetric("coords", "spin_equ,orbit_ecl,freq,fdots",
@@ -45,7 +63,7 @@ function results = Test3SkyMetric(varargin)
                                                "time_span", time_span,
                                                "detectors", detectors,
                                                "fiducial_freq", fiducial_freq,
-                                               "ptolemaic", ptolemaic);
+                                               "phase_model", "full");
 
   ## construct untransformed 3-sky metric
   metric3 = Construct3SkyMetrics(metric2p3, coordIDs2p3, "sky_coords", "equatorial");
@@ -62,24 +80,29 @@ function results = Test3SkyMetric(varargin)
          find(a_coordIDs3 == DOPPLERCOORD_F2DOT), ...
          find(a_coordIDs3 == DOPPLERCOORD_F3DOT)];
 
-  ## remove aligned-c direction to create reduced-aligned 3-sky metric
-  ra_metric3 = a_metric3;
-  ra_metric3(inc, :) = ra_metric3(:, inc) = [];
+  ## remove aligned-c direction to create sky-projected aligned 3-sky metric
+  spa_metric3 = a_metric3;
+  spa_metric3(inc, :) = spa_metric3(:, inc) = [];
+  assert(ina < inc);
+  assert(inb < inc);
+  spa_iff = iff;
+  spa_iff(spa_iff > inc) -= 1;
 
-  ## diagonally normalise reduced-aligned metric
-  [D_ra_metric3, Dnorm, iDnorm] = DiagonalNormaliseMetric(ra_metric3);
+  ## diagonally normalise sky-projected aligned metric
+  [D_spa_metric3, Dnorm, iDnorm] = DiagonalNormaliseMetric(spa_metric3);
 
-  ## compute Cholesky decomposition of diagonally-normalised reduced-aligned metric
-  CD_ra_metric3 = chol(D_ra_metric3);
+  ## compute Cholesky decomposition of diagonally-normalised sky-projected aligned metric
+  CD_spa_metric3 = chol(D_spa_metric3);
 
-  ## compute transform from surface of unit sphere to surface of reduced-aligned
+  ## compute transform from surface of unit sphere to surface of sky-projected aligned
   ## metric ellipsoid with maximum mismatch of 'max_mismatch'
-  onto_ra_metric3 = sqrt(max_mismatch) * Dnorm * inv(CD_ra_metric3);
+  onto_spa_metric3 = sqrt(max_mismatch) * Dnorm * inv(CD_spa_metric3);
 
   ## initalise result histograms
-  results.mu_ra_metric3_lgerr_H = newHist(1);
-  results.mu_a_metric3_lgerr_H = newHist(2);
-  results.mu_metric3_lgerr_H = newHist(1);
+  results.mu_spa_metric3_relax_H = newHist(1);
+  results.mu_a_metric3_relax_H = newHist(1);
+  results.mu_metricLPI_relax_H = newHist(1);
+  results.mu_metricLPII_relax_H = newHist(1);
 
   ## iterate over all trials in 'trial_block'-sized blocks
   trial_block = 1e5;
@@ -89,48 +112,47 @@ function results = Test3SkyMetric(varargin)
     endif
 
     ## create random point offsets on unit sphere surface
-    ra_dp = randn(size(ra_metric3, 1), trial_block);
-    N_ra_dp = norm(ra_dp, "cols");
-    for i = 1:size(ra_metric3, 1)
-      ra_dp(i, :) ./= N_ra_dp;
+    spa_dp = randn(size(spa_metric3, 1), trial_block);
+    N_spa_dp = norm(spa_dp, "cols");
+    for i = 1:size(spa_dp, 1)
+      spa_dp(i, :) ./= N_spa_dp;
     endfor
 
-    ## transform point offsets to surface of reduced-aligned
+    ## transform point offsets to surface of sky-projected aligned
     ## metric ellipsoid with maximum mismatch of 'max_mismatch'
-    ra_dp = onto_ra_metric3 * ra_dp;
+    spa_dp = onto_spa_metric3 * spa_dp;
 
-    ## compute mismatch in reduced-aligned metric
-    mu_ra_metric3 = dot(ra_dp, ra_metric3 * ra_dp);
+    ## compute mismatch in sky-projected aligned metric
+    mu_spa_metric3 = dot(spa_dp, spa_metric3 * spa_dp);
 
-    ## bin error in reduced-aligned mismatch compared to 'max_mismatch'
-    mu_ra_metric3_lgerr = log10(abs(mu_ra_metric3 - max_mismatch) ./ max_mismatch);
-    results.mu_ra_metric3_lgerr_H = addDataToHist(results.mu_ra_metric3_lgerr_H, mu_ra_metric3_lgerr(:), lgerr_H_dx);
+    ## create random sky points, depending on projection
+    switch sky_projection
+      case "orthographic"
 
-    ## create random radius and angle in aligned-a-b space (unit circle)
-    nr = rand(1, trial_block);
-    nth = rand(1, trial_block) * 2*pi;
+        ## create random point in unit disk
+        r = rand(1, trial_block);
+        th = rand(1, trial_block) * 2*pi;
+        x1 = [r .* cos(th); r .* sin(th)];
 
-    ## create random point in aligned sky sphere
-    nab1 = [nr .* cos(nth); nr .* sin(nth)];
-    ## use real when computing nc1 in case point has radius > 1
-    nc1 = real(sqrt(1 - sumsq(nab1, 1)));
+        ## add random offsets to create second random point
+        x2 = x1 + spa_dp([ina, inb], :);
 
-    ## create second random point by adding random offsets
-    nab2 = nab1 + ra_dp([ina, inb], :);
-    ## use real when computing nc2 in case point has radius > 1
-    nc2 = real(sqrt(1 - sumsq(nab2, 1)));
+        ## project onto sky upper hemisphere; use real() for computing
+        ## aligned-c component in case point has radius > 1
+        n1 = [x1; real(sqrt(1 - sumsq(x1, 1)))];
+        n2 = [x2; real(sqrt(1 - sumsq(x2, 1)))];
 
-    ## create point offsets in (non-reduced) aligned metric
+      otherwise
+        error("%s: unknown sky projection '%s'", funcName, sky_projection);
+    endswitch
+
+    ## create point offsets in (non-sky-projected) aligned metric
     a_dp = zeros(size(a_metric3, 1), trial_block);
-    a_dp([1:inc-1 inc+1:end], :) = ra_dp;
-    a_dp(inc, :) = nc2 - nc1;
+    a_dp([ina, inb, inc], :) = n2 - n1;
+    a_dp(iff, :) = spa_dp(spa_iff, :);
 
     ## compute mismatch in aligned metric
     mu_a_metric3 = dot(a_dp, a_metric3 * a_dp);
-
-    ## bin error in aligned mismatch compared to reduced-aligned mismatch, as function of aligned-a-b radius
-    mu_a_metric3_lgerr = log10(abs(mu_a_metric3 - mu_ra_metric3) ./ mu_ra_metric3);
-    results.mu_a_metric3_lgerr_H = addDataToHist(results.mu_a_metric3_lgerr_H, [mu_a_metric3_lgerr(:), nr(:)], lgerr_H_dx);
 
     ## compute inverse coordinate transform from aligned (residual) coordinates to untransformed 3-sky coordinates
     dp = zeros(size(a_dp));
@@ -140,9 +162,27 @@ function results = Test3SkyMetric(varargin)
     ## compute mismatch in untransformed metric
     mu_metric3 = dot(dp, metric3 * dp);
 
-    ## bin error in untransformed mismatch compared to aligned mismatch
-    mu_metric3_lgerr = log10(abs(mu_metric3 - mu_a_metric3) ./ mu_a_metric3);
-    results.mu_metric3_lgerr_H = addDataToHist(results.mu_metric3_lgerr_H, mu_metric3_lgerr(:), lgerr_H_dx);
+    ## bin relaxation of sky-projected aligned mismatch compared to untransformed mismatch
+    mu_spa_metric3_relax = mu_spa_metric3 ./ mu_metric3;
+    results.mu_spa_metric3_relax_H = addDataToHist(results.mu_spa_metric3_relax_H, mu_spa_metric3_relax(:), relax_H_dx);
+
+    ## bin relaxation of aligned mismatch compared to untransformed mismatch
+    mu_a_metric3_relax = mu_a_metric3 ./ mu_metric3;
+    results.mu_a_metric3_relax_H = addDataToHist(results.mu_a_metric3_relax_H, mu_a_metric3_relax(:), relax_H_dx);
+
+    ## compute mismatch in metric with linear phase model I
+    mu_metricLPI = dot(dp, metricLPI * dp);
+
+    ## bin relaxation of linear phase model I metric compared to untransformed mismatch
+    mu_metricLPI_relax = mu_metricLPI ./ mu_metric3;
+    results.mu_metricLPI_relax_H = addDataToHist(results.mu_metricLPI_relax_H, mu_metricLPI_relax(:), relax_H_dx);
+
+    ## compute mismatch in metric with linear phase model II
+    mu_metricLPII = dot(dp, metricLPII * dp);
+
+    ## bin relaxation of linear phase model II metric compared to untransformed mismatch
+    mu_metricLPII_relax = mu_metricLPII ./ mu_metric3;
+    results.mu_metricLPII_relax_H = addDataToHist(results.mu_metricLPII_relax_H, mu_metricLPII_relax(:), relax_H_dx);
 
     num_trials -= trial_block;
   endwhile
