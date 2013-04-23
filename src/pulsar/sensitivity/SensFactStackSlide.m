30157bcf70a7b991329ebee8552e27b1cc9d6ccd
Reinhard Prix <reinhard.prix@ligo.org>
2013-04-23 12:02:32 +0200

    extended SensFactStackSlide() to allow passing sky-position 'alpha', 'delta'

--- src/pulsar/sensitivity/SensFactStackSlide.m
+++ src/pulsar/sensitivity/SensFactStackSlide.m
@@ -7,18 +7,23 @@ function sensFact = SensFactStackSlide ( varargin )
   %% 'pFD':     	false-dismissal probability = 1 - pDet
   %% 'pFA':     	false-alarm probability (-ies) *per template* [can be a vector]
   %% 'detectors': 	string containing detector-network to use 'H'=Hanford, 'L'=Livingston, 'V'=Virgo
+  %% 'alpha': source right ascension in radians (default: all-sky)
+  %%  'delta': source declination (default: all-sky)
+  %%
 
   %% ----- parse commandline
   uvar = parseOptions ( varargin,
-                       {'Nseg', 'scalar', 1 },		%% number of StackSlide segments
-                       {'Tdata', 'scalar' },  		%% total amount of data used, in seconds
-                       {'misHist', 'Hist' },		%% mismatch histogram, produced using addDataToHist()
-                       {'pFD', 'scalar', 0.1},		%% false-dismissal probability = 1 - pDet
-                       {'pFA', 'vector' } ,		%% false-alarm probability (-ies) per template
-                       {'detectors', 'char', "HL" }	%% string containing detector-network to use 'H'=Hanford, 'L'=Livingston, 'V'=Virgo
+                       {"Nseg", 'scalar', 1 },
+                       {"Tdata", 'scalar' },
+                       {"misHist", 'Hist' },
+                       {"pFD", 'scalar', 0.1},
+                       {"pFA", 'vector' },
+                       {"detectors", 'char', "HL" },
+                       {"alpha", 'numeric,vector', [0, 2*pi]},
+                       {"delta", 'numeric,vector', [-1, 1]}
                        );
 
-  Rsqr = SqrSNRGeometricFactorHist("detectors", uvar.detectors, "mism_hgrm", uvar.misHist);
+  Rsqr = SqrSNRGeometricFactorHist("detectors", uvar.detectors, "mism_hgrm", uvar.misHist, "alpha", uvar.alpha, "sdelta", sin(uvar.delta) );
   rho = SensitivitySNR ( uvar.pFD, uvar.Nseg, Rsqr, "ChiSqr", "paNt", uvar.pFA );
 
   TdataSeg = uvar.Tdata / uvar.Nseg;
