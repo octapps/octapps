511446799c1f1d9091f7a620270a62aa22876e7d
Karl Wette <karl.wette@ligo.org>
2013-04-23 17:21:06 +0200

    loadEphemerides: common function for loading ephemerides from LALPulsar
    - defaults for Earth/Sun ephemeris files are now given *only* here

---
+++ src/pulsar/utilities/loadEphemerides.m
@@ -0,0 +1,41 @@
+## Copyright (C) 2013 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Load Earth and Sun ephemerides from LALPulsar.
+## Syntax:
+##   ephemerides = loadEphemerides(...)
+## where:
+##   ephemerides = structure containing ephemerides
+## Options:
+##   "earth_file": Earth ephemerides file (default: earth00-19-DE405.dat.gz)
+##   "sun_file": Sun ephemerides file (default: sun00-19-DE405.dat.gz)
+
+function ephemerides = loadEphemerides(varargin)
+
+  ## parse options
+  parseOptions(varargin,
+               {"earth_file", "char", "earth00-19-DE405.dat.gz"},
+               {"sun_file", "char", "sun00-19-DE405.dat.gz"},
+               []);
+
+  ## load ephemerides
+  try
+    ephemerides = InitBarycenter(earth_file, sun_file);
+  catch
+    error("%s: Could not load ephemerides", funcName);
+  end_try_catch
+
+endfunction
