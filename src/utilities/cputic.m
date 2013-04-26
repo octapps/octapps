8624fbd859d582d3be70a193fc8b5a6bbca99ec9
Karl Wette <karl.wette@ligo.org>
2013-04-26 17:37:53 +0200

    cputic, cputoc: utility functions for recording CPU time in named counters

---
+++ src/utilities/cputic.m
@@ -0,0 +1,58 @@
+## Copyright (C) 2013 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Sets or retrieves the value of a named CPU time counter.
+## Syntax:
+##   cputic("name");       ## sets the counter
+##   t = cputic("name");   ## retrieves the counter
+
+function varargout = cputic(name)
+
+  ## table of CPU time counters
+  persistent cputic_counters = [];
+
+  ## check input and output arguments
+  assert(ischar(name));
+  assert(nargout <= 1);
+
+  ## find index of named counter, or empty if it doesn't exist
+  if !isempty(cputic_counters)
+    i = find(strcmp(name, {cputic_counters.name}));
+  else
+    i = [];
+  endif
+
+  if nargout > 0
+
+    ## retrieves the counter, or error if it doesn't exist
+    if isempty(i)
+      error("%s: unknown CPU time counter '%s'", funcName, name);
+    else
+      varargout = {cputic_counters(i).t};
+    endif
+
+  else
+
+    ## set the counter, creating a new one if it doesn't exist
+    if isempty(i)
+      i = length(cputic_counters) + 1;
+    endif
+    cputic_counters(i).t = cputime();
+    cputic_counters(i).name = name;
+
+  endif
+
+endfunction
---
+++ src/utilities/cputoc.m
@@ -0,0 +1,34 @@
+## Copyright (C) 2013 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Prints the elapsed CPU time since a named CPU time counter was set.
+## Syntax:
+##   cputoc("name");
+
+function cputoc(name)
+
+  ## check input arguments
+  assert(ischar(name));
+
+  ## get CPU time now and at last counter set
+  t = cputime();
+  t0 = cputic(name);
+
+  ## print elapsed CPU time, without paging
+  page_screen_output(0, "local");
+  printf("Elapsed CPU time for counter '%s' = %0.3f\n", name, t - t0);
+
+endfunction
