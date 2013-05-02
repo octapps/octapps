6fb795eb2e653b55d54c0579f272a1a6bf49259f
David Keitel <david.keitel@aei.mpg.de>
2013-05-02 12:22:26 +0200

    Workaround for lalapps_dumpSFT bug

--- src/pulsar/lineveto/get_num_SFTs_from_file.m
+++ src/pulsar/lineveto/get_num_SFTs_from_file.m
@@ -1,8 +1,12 @@
 function num_SFTs = get_num_SFTs_from_file ( sftfile )
 
+ % safety measure to work around lalapps_dumpSFT bug: check if sftfile is a pattern matching several files, and if it is, just use the first one.
+ [status, output] = system(["find ", sftfile]);
+ sftfiles = strsplit(output,"\n");
+
  sft_counting_string_from_header = "Locator:";
 
- [status, output] = system(["lalapps_dumpSFT --SFTfiles=", sftfile, " | grep -c ", sft_counting_string_from_header]);
+ [status, output] = system(["lalapps_dumpSFT --SFTfiles=", sftfiles{1}, " | grep -c ", sft_counting_string_from_header]);
 
  num_SFTs = str2num(output);
 
