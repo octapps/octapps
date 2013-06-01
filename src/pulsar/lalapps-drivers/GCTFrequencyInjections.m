855b72c9e36d49cecb53d061d33413dae6f05eab
Karl Wette <karl.wette@ligo.org>
2013-06-01 15:06:57 +0200

    GCTFrequencyInjections: add options for frequency/spindown spacing and refinement

--- src/pulsar/lalapps-drivers/GCTFrequencyInjections.m
+++ src/pulsar/lalapps-drivers/GCTFrequencyInjections.m
@@ -21,48 +21,52 @@
 ## where:
 ##   results = structure containing results of injections
 ## Options:
-##   "SFT_timestamp_files":     cell array of SFT timestamp files; first two letters of
-##                              filenames must give detector name (e.g. H1)
-##   "GCT_num_segments":        number of segments
-##   "GCT_segment_span":        time span of each segment (seconds)
-##   "GCT_segment_file":        OR: segment file (columns: startGPS, endGPS, duration[h], NumSFTs)
-##   "ephem_year":              ephemeris year (e.g. 09-11)
+##   "SFT_timestamp_files":     comma-separated SFT timestamp files; first two letters of
+##                              filenames must give detector name (e.g. "H1...")
+##   "GCT_segment_file":        segment file (columns: startGPS, endGPS, duration[h], NumSFTs)
 ##   "noise_Sh":                noise Sh of generated SFTs
 ##   "inject_h0":               injection h0
-##   "inject_band":             injection SFT bandwidth
+##   "SFT_band":                injection SFT bandwidth
 ##   "alpha":                   right ascension (radians)
 ##   "delta":                   declination (radians)
+##   "ref_time":                GPS reference time
 ##   "freq":                    injection frequency (Hz)
+##   "dfreq":                   frequency spacing (Hz)
+##   "f1dot":                   first spindown start (Hz/s)
 ##   "f1dot_band":              first spindown band (Hz/s)
+##   "df1dot":                  first spindown spacing (Hz/s)
+##   "gamma1":                  first spindown refinement
+##   "f2dot":                   second spindown start (Hz/s^2)
 ##   "f2dot_band":              second spindown band (Hz/s^2)
-##   "f2dot_refine":            second spindown refinement factor (optional)
-##   "mismatch_per_dim":        mismatch per dimension
-##   "debug_level":             LAL debug level
+##   "df2dot":                  second spindown spacing (Hz/s^2)
+##   "gamma2":                  second spindown refinement
 
 function results = GCTFrequencyInjections(varargin)
 
   ## parse options
   opts = parseOptions(varargin,
-                      {"SFT_timestamp_files", "cell"},
-                      {"GCT_num_segments", "strictpos,integer", []},
-                      {"GCT_segment_span", "strictpos,real", []},
-                      {"GCT_segment_file", "char", []},
-                      {"ephem_year", "char"},
+                      {"SFT_timestamp_files", "char"},
+                      {"GCT_segment_file", "char"},
                       {"noise_Sh", "real,positive,scalar"},
                       {"inject_h0", "real,strictpos,scalar"},
-                      {"inject_band", "real,strictpos,scalar"},
+                      {"SFT_band", "real,strictpos,scalar"},
                       {"alpha", "real,scalar"},
                       {"delta", "real,scalar"},
+                      {"ref_time", "real,strictpos,scalar"},
                       {"freq", "real,strictpos,scalar"},
+                      {"dfreq", "real,strictpos,scalar"},
+                      {"f1dot", "real,scalar"},
                       {"f1dot_band", "real,positive,scalar"},
+                      {"df1dot", "real,strictpos,scalar"},
+                      {"gamma1", "real,strictpos,scalar"},
+                      {"f2dot", "real,scalar"},
                       {"f2dot_band", "real,positive,scalar"},
-                      {"f2dot_refine", "real,strictpos,scalar", []},
-                      {"mismatch_per_dim", "real,strictpos,scalar"},
-                      {"debug_level", "integer,scalar", 0},
+                      {"df2dot", "real,strictpos,scalar"},
+                      {"gamma2", "real,strictpos,scalar"},
                       []);
-  assert(isempty(GCT_segment_file) || (isempty(GCT_num_segments) && isempty(GCT_segment_span)));
 
-  ## check input
+  ## check for existence of files
+  SFT_timestamp_files = strsplit(SFT_timestamp_files, ",", true);
   for i = 1:length(SFT_timestamp_files)
     if !exist(SFT_timestamp_files{i}, "file")
       error("%s: file '%s' does not exist", funcName, SFT_timestamp_files{i});
@@ -77,31 +81,16 @@ function results = GCTFrequencyInjections(varargin)
   results.gitID = format_gitID(octapps_gitID());
   results.opts = opts;
 
-  ## determine reference time
-  sft_times = [];
-  for i = 1:length(SFT_timestamp_files)
-    sft_times = [sft_times; load(SFT_timestamp_files{i})(:,1)];
-  endfor
-  ref_time = mean([min(sft_times), max(sft_times)]);
-
-  ## average segment time span
-  if !isempty(GCT_segment_file)
-    segs = load(GCT_segment_file);
-    GCT_segment_span = mean(segs(:,2) - segs(:,1));
-  endif
-
   ## generate SFTs with injection
   MFD = struct;
-  MFD.v = debug_level;
   MFD.Alpha = alpha;
   MFD.Delta = delta;
   MFD.Freq = freq;
-  MFD.Band = inject_band;
+  MFD.Band = SFT_band;
   MFD.fmin = MFD.Freq - 0.5*MFD.Band;
   MFD.cosi = -1 + 2*rand();
-  MFD.ephemYear = ephem_year;
-  MFD.f1dot = -f1dot_band * rand();
-  MFD.f2dot = f2dot_band * rand();
+  MFD.f1dot = f1dot + f1dot_band * rand();
+  MFD.f2dot = f2dot + f2dot_band * rand();
   MFD.h0 = inject_h0;
   MFD.noiseSqrtSh = sqrt(noise_Sh);
   MFD.outSingleSFT = true;
@@ -116,18 +105,12 @@ function results = GCTFrequencyInjections(varargin)
     MFD.outSFTbname = SFT_files{i} = strcat(MFD.IFO, ".sft");
     MFD.timestampsFile = fullfile(".", SFT_timestamp_files{i});
     results.(strcat("MFD_", MFD.IFO)) = MFD;
-    runCode(MFD, "lalapps_Makefakedata_v4");
+    runCode(MFD, "lalapps_Makefakedata_v4", true);
 
   endfor
 
-  ## calculate frequency and spindown spacings from metric
-  i = 0:2;
-  gii = ( 4*pi^2 .* (1+i).^2 .* GCT_segment_span.^(2+2*i) ) ./ ( (3+2*i) .* factorial(2+i).^2 );
-  dfndot = 2 .* sqrt(mismatch_per_dim ./ gii);
-
   ## run HierarchSearchGCT (no mismatch)
   GCT = struct;
-  GCT.d = debug_level;
   GCT.DataFiles1 = SFT_files{1};
   for i = 2:length(SFT_files)
     GCT.DataFiles1 = strcat(GCT.DataFiles1, ";", SFT_files{i});
@@ -135,25 +118,18 @@ function results = GCTFrequencyInjections(varargin)
   GCT.skyRegion = sprintf("(%.16f,%.16f)", alpha, delta);
   GCT.gridType1 = 2;
   GCT.Freq = MFD.Freq;
-  GCT.dFreq = dfndot(1);
+  GCT.dFreq = dfreq;
   nFreqs = 10;
   GCT.FreqBand = 0.0;
-  GCT.ephemE = sprintf("earth%s.dat", MFD.ephemYear);
-  GCT.ephemS = sprintf("sun%s.dat", MFD.ephemYear);
   GCT.f1dot = MFD.f1dot;
   nf1dots = 10;
-  GCT.df1dot = min(dfndot(2), f1dot_band / nf1dots);
+  GCT.df1dot = df1dot;
   GCT.f1dotBand = 0.0;
   GCT.f2dot = MFD.f2dot;
   nf2dots = 10;
-  GCT.df2dot = min(dfndot(3), f2dot_band / nf2dots);
+  GCT.df2dot = df2dot;
   GCT.f2dotBand = 0.0;
-  if !isempty(GCT_segment_file)
-    GCT.segmentList = fullfile(".", GCT_segment_file);
-  else
-    GCT.nStacksMax = GCT_num_segments;
-    GCT.tStack = GCT_segment_span;
-  endif
+  GCT.segmentList = fullfile(".", GCT_segment_file);
   GCT.refTime = MFD.refTime;
   GCT.peakThrF = 0.0;
   GCT.SignalOnly = (noise_Sh == 0.0);
@@ -161,11 +137,10 @@ function results = GCTFrequencyInjections(varargin)
   GCT.semiCohToplist = true;
   GCT.fnameout = "GCT_Fstats";
   GCT.nCand1 = 1;
-  if !isempty(f2dot_refine)
-    GCT.gamma2Refine = f2dot_refine;
-  endif
+  GCT.gammaRefine = gamma1;
+  GCT.gamma2Refine = gamma2;
   results.GCT_no_mismatch = GCT;
-  runCode(GCT, "lalapps_HierarchSearchGCT");
+  runCode(GCT, "lalapps_HierarchSearchGCT", true);
 
   ## load no-mismatch Fstat results
   results.Fstats_no_mismatch = load(GCT.fnameout);
@@ -178,7 +153,7 @@ function results = GCTFrequencyInjections(varargin)
   GCT.f1dot += (-1 + 2*rand())*GCT.df1dot - 0.5*GCT.f1dotBand;
   GCT.f2dot += (-1 + 2*rand())*GCT.df2dot - 0.5*GCT.f2dotBand;
   results.GCT_with_mismatch = GCT;
-  runCode(GCT, "lalapps_HierarchSearchGCT");
+  runCode(GCT, "lalapps_HierarchSearchGCT", true);
 
   ## save with-mismatch Fstat result
   results.Fstats_with_mismatch = load(GCT.fnameout);
