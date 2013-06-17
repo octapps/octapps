b4b34c464151a409673e6b1dc734f65e1d6e3814
Karl Wette <karl.wette@ligo.org>
2013-06-17 12:22:32 +0200

    histRange: modify to return actual range covered by non-zero probability bins

--- src/histograms/@Hist/histRange.m
+++ src/histograms/@Hist/histRange.m
@@ -15,27 +15,36 @@
 ## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 ## MA  02111-1307  USA
 
-## Returns the range of a histogram class, which
-## is finite unless the histogram is empty.
+## Returns the range(s) of a histogram class, which are is finite
+## if the histogram is non-empty, and NaNs otherwise.
 ## Syntax:
-##   rng = histRange(hgrm)
+##   rng = histRange(hgrm, [kk = 1:dim])
 ## where:
 ##   hgrm = histogram class
-##   rng  = range of the histogram
+##   kk   = ranges to return; defaults to all
+##   rng  = range(s) of the histogram
 
-function rng = histRange(hgrm)
+function rng = histRange(hgrm, kk = [])
 
   ## check input
   assert(isHist(hgrm));
   dim = length(hgrm.bins);
+  if isempty(kk)
+    kk = 1:dim;
+  else
+    assert(all(1 <= kk && kk <= length(hgrm.bins)));
+  endif
 
   ## return range
-  rng = zeros(dim, 2);
-  for k = 1:dim
-    if length(hgrm.bins{k}) > 2
-      rng(k, :) = hgrm.bins{k}([2, end-1]);
+  rng = zeros(length(kk), 2);
+  for i = 1:length(kk)
+    h = contractHist(hgrm, kk(i));
+    ii = find(h.counts > 0);
+    if isempty(ii)
+      rng(i, :) = NaN;
     else
-      rng(k, :) = hgrm.bins{k};
+      rng(i, 1) = h.bins{1}(min(ii));
+      rng(i, 2) = h.bins{1}(max(ii)+1);
     endif
   endfor
 
