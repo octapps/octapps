f96e83dad02f0c247b240097a7f2667229e09b43
Karl Wette <karl.wette@ligo.org>
2013-08-12 18:04:22 +0200

    Rename percentileOfHist() to quantileFuncOfHist()
    - http://en.wikipedia.org/wiki/Quantile_function
    - also add a test script

--- src/histograms/quantileFuncOfHist.m
+++ src/histograms/quantileFuncOfHist.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2010 Karl Wette
+## Copyright (C) 2013 Karl Wette
 ##
 ## This program is free software; you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
@@ -15,20 +15,20 @@
 ## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
 ## MA  02111-1307  USA
 
-## Returns the percentile(s) of a histogram.
+## Evaluates the quantile function 'Q(p)' of a histogram at a given point 'p'.
 ## Syntax:
-##   percentile = percentileOfHist(hgrm, pc, [k = 1])
+##   Q = quantileFuncOfHist(hgrm, p, [k = 1])
 ## where:
 ##   hgrm = histogram class
-##   pc   = percentile, in the range (0, 1)
-##   k    = dimension to calculate percentile(s) over
+##   p    = quantile probability, in the range (0, 1)
+##   k    = dimension over which to to evaluate quantile function
 
-function percentile = percentileOfHist(hgrm, pc, k = 1)
+function Q = quantileFuncOfHist(hgrm, p, k = 1)
 
   ## check input
   assert(isHist(hgrm));
   dim = histDim(hgrm);
-  assert(isscalar(pc) && 0 < pc && pc < 1);
+  assert(isscalar(p) && 0 < p && p < 1);
   assert(isscalar(k) && 1 <= k && k <= dim);
 
   ## get probability densities
@@ -56,17 +56,25 @@ function percentile = percentileOfHist(hgrm, pc, k = 1)
   ## normalise probabilities
   cumprob ./= norm;
 
-  ## get lower and upper cumulative probabilities containing pc
+  ## get lower and upper cumulative probabilities containing 'p'
   [subs{1:length(siz)}] = ndgrid(arrayfun(@(n) 1:n, ifelse(1:dim == k, 1, siz), "UniformOutput", false){:});
-  subs{k} = subsk = sum(cumprob < pc, k);
+  subs{k} = subsk = sum(cumprob < p, k);
   cumproblower = cumprob(sub2ind(siz, subs{:}));
   subs{k} += 1;
   cumprobupper = cumprob(sub2ind(siz, subs{:}));
 
-  ## calculate percentile(s)
+  ## calculate quantile function
   [xl, dx] = histBins(hgrm, k, "finite", "lower", "width");
   xlk = reshape(xl(subsk), size(cumproblower));
   dxk = reshape(dx(subsk), size(cumproblower));
-  percentile = squeeze(xlk + dxk .* (pc - cumproblower) ./ (cumprobupper - cumproblower));
+  Q = squeeze(xlk + dxk .* (p - cumproblower) ./ (cumprobupper - cumproblower));
 
 endfunction
+
+
+## generate standard normal histogram and calculate some quantiles
+%!test
+%! hgrm = createGaussianHist(0, 1, "err", 1e-3, "binsize", 0.1);
+%! assert(abs(quantileFuncOfHist(hgrm, normcdf(-1)) - (-1)) < 0.005);
+%! assert(abs(quantileFuncOfHist(hgrm, normcdf( 0)) - ( 0)) < 0.005);
+%! assert(abs(quantileFuncOfHist(hgrm, normcdf(+1)) - (+1)) < 0.005);
