09011889ed9e650dca9adbfc1c7ee9c1a15a4edb
Karl Wette <karl.wette@ligo.org>
2013-09-19 17:29:23 +0200

    createDeltaHist: function which returns histogram representing a delta function

---
+++ src/histograms/createDeltaHist.m
@@ -0,0 +1,29 @@
+## Copyright (C) 2013 Karl Wette
+## 
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Creates a histogram representing a delta function at a given value
+## Usage:
+##   hgrm = createDeltaHist(x0)
+## where:
+##   hgrm = delta function histogram
+##   x0   = location of delta function
+
+function hgrm = createDeltaHist(x0)
+
+  ## create histogram of delta function
+  hgrm = addDataToHist(Hist(1, [x0, x0+eps]), x0);
+
+endfunction
