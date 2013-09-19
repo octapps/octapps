8281e5f53787f91896bfbf0a9cbea964ed4432f1
Karl Wette <karl.wette@ligo.org>
2013-09-19 17:12:30 +0200

    Update documentation of createGaussianHist()

--- src/histograms/createGaussianHist.m
+++ src/histograms/createGaussianHist.m
@@ -1,71 +1,63 @@
-%% Returns a normalized histogram representing a Gaussian pdf with given mean and standard deviation
-%%
-%% Syntax:
-%%   hgrm = createGaussianHist (M, S, "key1", val1, ... )
-%% where:
-%%   hgrm = returned histogram class
-%%   M    = mean of the Gaussian distribution
-%%   V    = standard deviation
-%%
-%%   optional allowed keywords are
-%%   "err" = convergence requirement on histogram, default err = 1e-2
-%%   "binsize"  = histogram bin-size, default binsize = 0.01
-%%
+## Copyright (C) 2011 Reinhard Prix
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
 
-%%
-%%  Copyright (C) 2011 Reinhard Prix
-%%
-%%  This program is free software; you can redistribute it and/or modify
-%%  it under the terms of the GNU General Public License as published by
-%%  the Free Software Foundation; either version 2 of the License, or
-%%  (at your option) any later version.
-%%
-%%  This program is distributed in the hope that it will be useful,
-%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%  GNU General Public License for more details.
-%%
-%%  You should have received a copy of the GNU General Public License
-%%  along with with program; see the file COPYING. If not, write to the
-%%  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-%%  MA  02111-1307  USA
-%%
+## Creates a histogram representing a Gaussian PDF with given mean and standard deviation
+## Usage:
+##   hgrm = createGaussianHist("opt", val, ...)
+## where:
+##   hgrm = Gaussian PDF histogram
+## Options are:
+##   "mean":     mean of the Gaussian distribution
+##   "std":      standard deviation
+##   "err":      convergence requirement on histogram (default = 1e-2)
+##   "binsize":  histogram bin-size (default = "std" / 10)
 
-function hgrm = createGaussianHist ( M, S, varargin )
+function hgrm = createGaussianHist ( varargin )
 
-  %% check input arguments
-  if ( !exist("M") || !exist("S") )
-    error ("%s: Need to provide arguments mean 'M' and standard deviation 'S'\n", funcName );
-  endif
-  if ( !isscalar(M) || !isscalar(S) )
-    error ("%s: Input arguments for mean and standard deviation must be scalars!\n", funcName );
+  ## parse optional keywords
+  uvar = parseOptions(varargin,
+                      {"mean", "real,scalar"},
+                      {"std", "real,strictpos,scalar"},
+                      {"err", "real,scalar", 1e-2},
+                      {"binsize", "real,scalar", []});
+  if isempty(uvar.binsize)
+    uvar.binsize = uvar.std / 10.0;
   endif
 
-  %% parse optional keywords
-  parseOptions(varargin,
-               {"err", "numeric,scalar", 1e-2},
-               {"binsize", "numeric,scalar", S / 10.0});
+  ## create 1D histogram class
+  hgrm = Hist( 1, {"lin", "dbin", uvar.binsize} );
 
-  %% create 1D histogram class
-  hgrm = Hist( 1, {"lin", "dbin", binsize} );
-
-  %% iterate, adding N samples every round, and continue until histogram has converged
-  %% to within the given tolerance of 'err'
+  ## iterate, adding N samples every round, and continue until histogram has converged
+  ## to within the given tolerance of 'err'
   N = 1e6;
   do
-    %% generate values of ap, ax, Fp, and Fx
-    newsamples = normrnd ( M, S, N, 1);	%% N-vector of Gaussian random draws
+    ## generate values of ap, ax, Fp, and Fx
+    newsamples = normrnd ( uvar.mean, uvar.std, N, 1);     ## N-vector of Gaussian random draws
 
-    %% add new values to histogram
+    ## add new values to histogram
     oldhgrm = hgrm;
     hgrm = addDataToHist ( hgrm, newsamples );
 
-    %% calculate difference between old and new histograms
+    ## calculate difference between old and new histograms
     histerr = histDistance ( hgrm, oldhgrm );
 
-    %% continue until error is small enough
-    %% (exit after 1 iteration if all parameters are constant)
-  until histerr < err
+    ## continue until error is small enough
+    ## (exit after 1 iteration if all parameters are constant)
+  until histerr < uvar.err
 
 endfunction
 
