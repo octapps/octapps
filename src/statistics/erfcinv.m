7808b466c5045ef6e2b11c55a4c1e75549d15b23
Karl Wette <karl.wette@ligo.org>
2013-10-27 16:10:54 +0100

    erfcinv: add asymptotic expression for small x

--- src/statistics/erfcinv.m
+++ src/statistics/erfcinv.m
@@ -1,28 +1,53 @@
-%% ret = erfcinv ( x )
-%%
-%% inverse complementary error-function
-%% trivial implementation of erfc^-1(x), which is simply erf^-1(1 - x)
-%%
+## Copyright (C) 2006 Reinhard Prix
+## Copyright (C) 2013 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
 
-%%
-%% Copyright (C) 2006 Reinhard Prix
-%%
-%%  This program is free software; you can redistribute it and/or modify
-%%  it under the terms of the GNU General Public License as published by
-%%  the Free Software Foundation; either version 2 of the License, or
-%%  (at your option) any later version.
-%%
-%%  This program is distributed in the hope that it will be useful,
-%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
-%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%%  GNU General Public License for more details.
-%%
-%%  You should have received a copy of the GNU General Public License
-%%  along with with program; see the file COPYING. If not, write to the
-%%  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-%%  MA  02111-1307  USA
-%%
+## -*- texinfo -*-
+## @deftypefn {Function File} {} erfcinv(@var{X})
+## Compute the inverse complementary error function, i.e., @var{Y} such that
+##
+## @example
+##  erfc(@var{y}) == @var{x}
+## @end example
+##
+## An asymptotic expression is used for small @var{X}.
+## @end deftypefn
 
-function ret = erfcinv ( x )
-  ret = erfinv ( 1 - x );
+function y = erfcinv(x)
+
+  ## initialise output
+  y = zeros(size(x));
+
+  ## for large x, use erfinv
+  ii = (x < 1e-14);
+  if any(!ii(:))
+    y(!ii) = erfinv( 1 - x(!ii) );
+  endif
+
+  ## use asymptotic expression for small x:
+  ## from http://dlmf.nist.gov/7.17#iii
+  if any(ii(:))
+    x = x(ii(:));
+    u = -2 ./ log(pi .* x.^2 .* log(1./x));
+    v = log(log(1./x)) - 2 + log(pi);
+    a2 = (1/8) .* v;
+    a3 = (-1/32) .* (v.^2 + 6.*v - 6);
+    a4 = (1/384) .* (4.*v.^3 + 27.*v.^2 + 108.*v - 300);
+    y(ii) = u.^(-1/2) + a2.*u.^(3/2) + a3.*u.^(5/2) + a4.*u.^(7/2);
+  endif
+
 endfunction
