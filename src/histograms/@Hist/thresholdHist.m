03f8563d55c2bdc08e256761c5c9770fae04a619
Karl Wette <karl.wette@ligo.org>
2016-04-05 13:32:57 +0200

    Revert "thresholdHist(): trim histogram of lower/upper bins with no counts"
    This reverts commit 61866b55531390eb5f2851635bffe0edf5e57158.

--- src/histograms/@Hist/thresholdHist.m
+++ src/histograms/@Hist/thresholdHist.m
@@ -14,9 +14,7 @@
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ## Set a threshold on the histogram probability of each bin; bins with
-## probability below the threshold have their count set to zero. The
-## histogram is then trimmed: in each dimension, the lowest/highest bins
-## are removed until such bins contain at least one count above threshold.
+## probability below the threshold have their count set to zero.
 ## Usage:
 ##   thgrm = thresholdHist(hgrm, pth)
 ## where:
@@ -28,46 +26,12 @@ function hgrm = thresholdHist(hgrm, pth)
 
   ## check input
   assert(isHist(hgrm));
-  assert(isscalar(pth) && 0 <= pth && pth <= 1);
-  dim = length(hgrm.bins);
+  assert(isscalar(pth) && 0 < pth && pth <= 1);
+
+  ## get histogram probabilities
+  prob = histProbs(hgrm);
 
   ## zero count in bins with probability below threshold
-  prob = histProbs(hgrm);
-  hgrm.counts(find(prob <= pth)) = 0;
-
-  ## trim histograms
-  newbins = {};
-  for k = 1:dim
-
-    ## permute dimension k to beginning of array, then flatten other dimensions
-    prob = histProbs(hgrm);
-    perm = [k 1:(k-1) (k+1):max(dim,length(size(prob)))];
-    prob = permute(prob, perm);
-    siz = size(prob);
-    prob = reshape(prob, siz(1), []);
-
-    ## find contiguous area of bins above threshold in this dimension
-    ii = repmat(reshape(1:size(prob, 1), [], 1), 1, size(prob, 2));
-    ii(prob <= pth) = NaN;
-    iimin = min(ii(:));
-    iimax = max(ii(:));
-
-    ## select bin range to resize histogram to
-    newbins{k} = hgrm.bins{k}([iimin, iimax+1]);
-
-  endfor
-  hgrm = restrictHist(hgrm, newbins{:});
+  hgrm.counts(find(prob < pth)) = 0;
 
 endfunction
-
-
-%!test
-%!  hgrm = Hist(3, {"lin", "dbin", 1}, {"lin", "dbin", 1}, {"lin", "dbin", 1});
-%!  hgrm = addDataToHist(hgrm, [-4.5:4.5; 3.5:12.5; 10.5:19.5]');
-%!  rng = histRange(hgrm);
-%!  hgrm2 = thresholdHist(hgrm, 0);
-%!  for k = 1:3
-%!    [xl, xh] = histBins(hgrm2, k, "lower", "upper");
-%!    assert(rng(k, 1) == xl(2));
-%!    assert(rng(k, 2) == xh(end-1));
-%!  endfor
