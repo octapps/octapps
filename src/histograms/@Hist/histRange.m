61fe54b6b349ea87b7aff3b24f7c6b6ea3cac01a
Karl Wette <karl.wette@ligo.org>
2016-04-09 19:50:02 +0200

    histRange: add option to only return range of finite bins

--- src/histograms/@Hist/histRange.m
+++ src/histograms/@Hist/histRange.m
@@ -19,22 +19,34 @@
 ## if the histogram is non-empty and NaNs otherwise, and the
 ## number of bins in each range.
 ## Syntax:
-##   [rng, nbins] = histRange(hgrm, [kk = 1:dim])
+##   [rng,  nbins ] = histRange(hgrm, [kk = 1:dim])
+##   [frng, fnbins] = histRange(hgrm, [kk = 1:dim], "finite")
 ## where:
-##   hgrm  = histogram class
-##   kk    = dimensions for which to return ranges; defaults to all
-##   rng   = ranges of the histogram
-##   nbins = number of bins in each range
+##   hgrm   = histogram class
+##   kk     = dimensions for which to return ranges; defaults to all
+##   rng    = ranges of the histogram
+##   nbins  = number of bins in each range
+##   frng   = finite ranges of the histogram
+##   fnbins = number of finite bins in each range
 
-function [rng, nbins] = histRange(hgrm, kk = [])
+function [rng, nbins] = histRange(hgrm, varargin)
 
   ## check input
+  narginchk(1, 3)
   assert(isHist(hgrm));
   dim = length(hgrm.bins);
-  if isempty(kk)
-    kk = 1:dim;
+  if ischar(varargin{end})
+    assert(strcmp(varargin{end}, "finite"));
+    fretn = true;
+    varargin = varargin(1:end-1);
   else
+    fretn = false;
+  endif
+  if length(varargin) > 1
+    kk = varargin{end};
     assert(all(1 <= kk && kk <= length(hgrm.bins)));
+  else
+    kk = 1:dim;
   endif
 
   ## return range
@@ -42,6 +54,9 @@ function [rng, nbins] = histRange(hgrm, kk = [])
   nbins = zeros(length(kk), 1);
   for i = 1:length(kk)
     h = contractHist(hgrm, kk(i));
+    if fretn
+      h.counts(1) = h.counts(end) = 0;
+    endif
     ii = find(h.counts > 0);
     if isempty(ii)
       rng(i, :) = NaN;
