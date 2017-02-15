d61bbf7f3e6fb3a7998f3dd1dfd123d1289e94b2
Karl Wette <karl.wette@ligo.org>
2017-02-15 16:38:31 +0100

    Add function fevalstruct() for evaluating functions with keyword-value arguments
    - Slightly cleaner replacement for struct2varargin(), which is now deprecated
    - Can also strip out empty arguments if "stripempty" is set to true

---
+++ src/command-line/fevalstruct.m
@@ -0,0 +1,53 @@
+## Copyright (C) 2017 Karl Wette
+## Copyright (C) 2015 Reinhard Prix
+##
+## This program is free software; you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn  {Function File} fevalstruct (@var{name}, @var{argstruct})
+## @deftypefnx {Function File} fevalstruct (@dots{}, @var{property}, @var{value}, @dots{})
+## Evaluate the function named @var{name}, using the keyword-value
+## arguments given by @var{argstruct}.
+##
+## If @var{property} "stripempty" is true, empty fields are first removed from @var{argstruct}.
+## @seealso{feval}
+## @end deftypefn
+
+function varargout = fevalstruct(name, argstruct, varargin)
+
+  ## parse input
+  assert(isstruct(argstruct));
+  parseOptions(varargin,
+               {"stripempty", "logical,scalar", false},
+               []);
+
+  ## strip empty fields, if requested
+  if stripempty
+    keys = fieldnames(argstruct);
+    for n = 1:length(keys)
+      if isempty(getfield(argstruct, keys{n}))
+        argstruct = rmfield(argstruct, keys{n});
+      endif
+    endfor
+  endif
+
+  ## create keyword-value arguments
+  keys = fieldnames(argstruct);
+  vals = struct2cell(argstruct);
+  keyvals = [ keys'; vals' ];
+
+  ## evaluate function
+  [varargout{1:nargout}] = feval(name, keyvals{:});
+
+endfunction
