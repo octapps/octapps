75b6d597781cf5f65295e5b0240d1d869fb820ba
Karl Wette <karl.wette@ligo.org>
2017-03-29 01:05:51 +0200

    Add writeConfigFile: write a .ini style configuration file from a struct

---
+++ src/file-handling/writeConfigFile.m
@@ -0,0 +1,103 @@
+## Copyright (C) 2017 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <http://www.gnu.org/licenses/>.
+
+## Write a .ini style configuration file from a struct.
+## Usage:
+##   writeConfigFile(file, cfg)
+## where
+##   file = configuration file
+##   cfg  = configuration file contents
+
+function writeConfigFile(file, cfg)
+
+  ## check input
+  assert(ischar(file));
+  assert(isstruct(cfg));
+
+  ## write to new file
+  new_file = strcat(file, ".writeConfigFile.tmp");
+  f = fopen(new_file, "w");
+  assert(f >= 0, "%s: could not open file '%s'", funcName, new_file);
+
+  ## write configuration file
+  sections = fieldnames(cfg);
+  for i = 1:length(sections)
+
+    ## write section heading
+    if i > 1
+      fprintf(f, "\n");
+    endif
+    comment = getoptfield("", cfg, sprintf("_comment_%s", sections{i}));
+    if !isempty(comment)
+      assert(any(comment(1) == ";#"), "%s: invalid comment '%s'", funcName, comment);
+      fprintf(f, "%s\n", comment);
+    endif
+    fprintf(f, "[%s]\n", sections{i});
+
+    ## write section names and values
+    names = fieldnames(cfg.(sections{i}));
+    names = names(cellfun(@(name) name(1) != "_", names));
+    for j = 1:length(names)
+      cfg_section = cfg.(sections{i});
+
+      ## write comment
+      comment = getoptfield("", cfg_section, sprintf("_comment_%s", names{j}));
+      if !isempty(comment)
+        assert(any(comment(1) == ";#"), "%s: invalid comment '%s'", funcName, comment);
+        fprintf(f, "%s\n", comment);
+      endif
+
+      ## write name
+      fprintf(f, "%s = ", names{j});
+
+      ## write value
+      value = cfg_section.(names{j});
+      if ischar(value)
+        fprintf(f, "%s", value);
+      else
+        valuestr = getoptfield("", cfg_section, sprintf("_str_%s", names{j}));
+        if !isempty(valuestr) && str2double(valuestr) != value
+          valuestr = "";
+        endif
+        if !isempty(valuestr)
+          fprintf(f, "%s", valuestr);
+        else
+          valuefmt = strcat("%", getoptfield("g", cfg_section, sprintf("_format_%s", names{j})));
+          fprintf(f, valuefmt, value)
+        endif
+      endif
+        
+      ## write inline comment
+      inlinecomment = getoptfield("", cfg_section, sprintf("_inlinecomment_%s", names{j}));
+      if !isempty(inlinecomment)
+        assert(any(inlinecomment(1) == ";#"), "%s: invalid inline comment '%s'", funcName, inlinecomment);
+        fprintf(f, "   %s\n", inlinecomment);
+      else
+        fprintf(f, "\n");
+      endif
+
+    endfor
+
+  endfor
+
+  ## close file
+  fclose(f);
+
+  ## replace old configuration file
+  [status, msg] = movefile(new_file, file, "f");
+  assert(status == 1, "%s: could not replace '%s' with '%s': %s", funcName, file, new_file, msg);
+
+endfunction
