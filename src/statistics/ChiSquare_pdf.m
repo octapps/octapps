eae53ebf713652104ad1e2ed653ed7024f7a56c0
Karl Wette <karl.wette@ligo.org>
2017-04-28 15:54:55 +0200

    ChiSquare_pdf(): some minor improvements to Christoph's previous patch
    - Check if logp_ts = logp_t1 + logp_t2 + logp_t3 is not finite as well as
      positive, since logp_t3 can also be NaN for large values of x and lambda
    - Simplify some of the index logic: by &ing kk with ii, we don't have to use
      both when indexing
    - Add some test cases

--- src/statistics/ChiSquare_pdf.m
+++ src/statistics/ChiSquare_pdf.m
@@ -49,27 +49,26 @@ function p = ChiSquare_pdf(x, k, lambda=0)
 
   ## compute the non-central chi^2 PDF in the general case
   ii = !isfinite(p);
+  normal_approx = 0;
   if any(ii(:))
 
      ## compute terms in logarithm of PDF
-     logp_t1 = logp_t2 = nu = z = logp_t3 = zeros(size(p));
+     logp_t1 = logp_t2 = nu = z = logp_t3 = logp_ts = zeros(size(p));
      logp_t1(ii) = -log(2) - 0.5.*( x(ii) + lambda(ii) );
      logp_t2(ii) = (k(ii)./4 - 0.5).*log( x(ii) ./ lambda(ii) );
      nu(ii) = k(ii)./2 - 1;
      z(ii) = sqrt(x(ii) .* lambda(ii));
      logp_t3(ii) = log( besseli( nu(ii), z(ii) ) );
+     logp_ts(ii) = logp_t1(ii) + logp_t2(ii) + logp_t3(ii);
 
      ## compute PDF
-     p(ii) = exp( logp_t1(ii) + logp_t2(ii) + logp_t3(ii) );
+     p(ii) = exp( logp_ts(ii) );
 
-     ## approximate by normal distribution if chi square failed
-     approx = 0;
-     phelp = p(ii);
-     if any(logp_t1(ii) + logp_t2(ii) + logp_t3(ii) > 0)
-       kk = (logp_t1(ii) + logp_t2(ii) + logp_t3(ii) > 0);
-       phelp(kk) = normpdf(x(ii)(kk), k(ii)(kk) + lambda(ii)(kk), sqrt( 2.* ( k(ii)(kk) + 2.*lambda(ii)(kk) ) ));
-       p(ii) = phelp;
-       approx = 1;
+     ## approximate by normal distribution if chi^2 failed
+     kk = ii & ( !isfinite(logp_ts) | (logp_ts > 0) );
+     if any(kk)
+       p(kk) = normpdf(x(kk), k(kk) + lambda(kk), sqrt( 2.* ( k(kk) + 2.*lambda(kk) ) ));
+       normal_approx = 1;
      endif
 
   endif
@@ -77,10 +76,16 @@ function p = ChiSquare_pdf(x, k, lambda=0)
   ## check for valid probabilities
   ii = !isfinite(p) | (p < 0) | (p > 1);
   for i = find(ii(:)')
-    warning( "%s: got p=%g for x=%g, k=%g, lambda=%g, approx=%f", funcName, p(i), x(i), k(i), lambda(i), approx );
+    warning( "%s: got p=%g for x=%g, k=%g, lambda=%g, normal_approx=%d", funcName, p(i), x(i), k(i), lambda(i), normal_approx );
   endfor
   if any(ii(:))
     error( "%s: got invalid probabilities for some inputs", funcName );
   endif
 
 endfunction
+
+
+%!assert(ChiSquare_pdf(10^-2, 10^-2, 1), 0.44224, 1e-3)
+%!assert(ChiSquare_pdf(10^+2, 10^+2, 2), 0.027895, 1e-3)
+%!assert(ChiSquare_pdf(10^10, 10^10, 3), 2.8209e-06, 1e-3)
+%!assert(ChiSquare_pdf(10^30, 10^30, 4), 2.8209e-16, 1e-3)
