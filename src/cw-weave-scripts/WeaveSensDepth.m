60a6c54d555c36bb6b5663f1913e1657fe495fdc
Reinhard Prix <reinhard.prix@ligo.org>
2018-01-16 17:51:10 +0100

    make WeaveSensDepth() return the mismatch histogram as well
    - just adds the optional return argument
    - can be useful, also for verification purposes

--- src/cw-weave-scripts/WeaveSensDepth.m
+++ src/cw-weave-scripts/WeaveSensDepth.m
@@ -51,7 +51,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-function depth = WeaveSensDepth(varargin)
+function [depth, mismatch_hgrm] = WeaveSensDepth(varargin)
 
   ## parse options
   parseOptions(varargin,
