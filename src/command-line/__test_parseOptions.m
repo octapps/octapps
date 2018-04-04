b93cd9289cd6bc8d5afaf664a127e86ca138d186
Karl Wette <karl.wette@ligo.org>
2018-04-04 15:54:40 +1000

    src/command-line: add __test_parseOptions() function for tests

---
+++ src/command-line/__test_parseOptions.m
@@ -0,0 +1,37 @@
+## Copyright (C) 2018 Karl Wette
+##
+## This program is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 2 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with with program; see the file COPYING. If not, write to the
+## Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+## MA  02111-1307  USA
+
+## Helper function for testing parseOptions() and related functions.
+
+function optstr = __test_parseOptions(varargin)
+
+  ## parse options
+  opts = parseOptions(varargin,
+                      {"real_strictpos_scalar", "real,strictpos,scalar"},
+                      {"integer_vector", "integer,vector"},
+                      {"string", "char"},
+                      {"cell", "cell", {1;1;1}},
+                      {"twobytwo", "rows:2,cols:2,+exactlyone:cell", eye(2)},
+                      []);
+
+  ## return string of all options
+  optstr = stringify(opts);
+
+endfunction
+
+%!test
+%!  disp("this function is only used to test parseOptions() and related functions");
