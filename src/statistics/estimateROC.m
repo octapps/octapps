bbcfb7720bedb3724d6738aee3f5b39fe949f167
Karl Wette <karl.wette@ligo.org>
2018-04-04 16:58:28 +1000

    Update/add tests for estimateROC()

--- src/statistics/estimateROC.m
+++ src/statistics/estimateROC.m
@@ -36,6 +36,12 @@ function [pDet_MPE, pDet_Lower, pDet_Upper, pFA_MPE, pFA_Lower, pFA_Upper] = est
 
 endfunction
 
+%!test
+%!  Ntrials_N = 600; Ntrials_S = 300;
+%!  stat_S = normrnd ( 1, 1, 1, Ntrials_S );
+%!  stat_N = normrnd ( 0, 1, 1, Ntrials_N );
+%!  pFA = linspace ( 0, 1, 15 );
+%!  [pDet_MPE, pDet_Lower, pDet_Upper, pFA_MPE, pFA_Lower, pFA_Upper] = estimateROC ( stat_N, stat_S, pFA, confidence=0.9545 );
 %!demo
 %!  Ntrials_N = 600; Ntrials_S = 300;
 %!  stat_S = normrnd ( 1, 1, 1, Ntrials_S );
