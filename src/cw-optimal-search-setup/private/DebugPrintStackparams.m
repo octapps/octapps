4af90c28a7e5934afbe93f6f89fa3c7ddc80ef7d
Karl Wette <karl.wette@ligo.org>
2020-05-28 18:05:45 +1000

    DebugPrintStackparams.m: use consistent whitespace

--- src/cw-optimal-search-setup/private/DebugPrintStackparams.m
+++ src/cw-optimal-search-setup/private/DebugPrintStackparams.m
@@ -45,7 +45,7 @@ function DebugPrintStackparams ( level, stackparams )
   if ( isfield ( stackparams, "mf" ) ) mInc = stackparams.mf; else mInc = stackparams.mInc; endif
 
   ## ----- output this
-  fprintf ( stderr, "{Nseg = %6.1f, Tseg = %7.2f d, Tobs = %7.2f d, mCoh = %-7.2g, mInc = %-7.2g} : dCC0=%+6.0e : %s=%+8.2e",
+  fprintf ( stderr, "{Nseg = %6.1f, Tseg = %7.2f d, Tobs = %7.2f d, mCoh = %7.2g, mInc = %7.2g} : dCC0=%+6.0e : %s=%+8.2e",
             stackparams.Nseg, stackparams.Tseg/DAYS, stackparams.Nseg * stackparams.Tseg/DAYS, mCoh, mInc, dC, L0name, L0 );
   if ( isfield ( stackparams, "DepthNLM" ) )
     fprintf ( stderr, " ==> DepthNLM = %6.1f", stackparams.DepthNLM );
